22db6899b22de247ebc1e29368a0a6cde9dbb701
YusufGencay <yusuf0gencay@gmail.com>
2023-02-08 21:13:24 -0500

    Initial commit: I created SDET Project and Hellow World class.

---
+++ src/Week1/HelloWorld.java
@@ -0,0 +1,10 @@
+package Week1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hellow World!!!");
+        System.out.println("Welcome to SDET Course");
+        System.out.print(" This line is starting with the previous line");
+
+    }
+}
