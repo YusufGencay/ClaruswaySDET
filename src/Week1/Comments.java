d533798ec6a3d92e7ed08286ae8bad68bcdae3ed
YusufGencay <yusuf0gencay@gmail.com>
2023-02-08 21:58:38 -0500

    I added Comments class.

---
+++ src/Week1/Comments.java
@@ -0,0 +1,23 @@
+package Week1;
+
+public class Comments {
+    // This is the first type of comment starts with //
+    // This is comment
+
+    /* I can write multiple lines of comments in here
+    This is my first line
+    and this is my second line
+    and this is will be the third line.
+     */
+
+    /**
+     * This is another comment with multi lines
+     * this is the second line
+     * Comment 3rd line
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
